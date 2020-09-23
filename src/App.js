89a1cb8f6658f9560950e4b9c283e63443faaaf7
dereknahman <44810431+dereknahman@users.noreply.github.com>
2020-09-23 20:25:06 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Cat from "./components/Cat/index";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  state = {
+    cats: [
+      { name: "Chashu", age: 2, activity: "rolling around" },
+      { name: "Nori", age: 4, activity: "snoozing" },
+      { name: "Fat Tony", age: 5, activity: "playing tetris" },
+      { name: "Big Trish", age: 10, activity: "antifascist organising" },
+      { name: "Gerald", age: 14, activity: "licking my butt" },
+    ],
+    // buttonTextValue: "Change my name!",
+    cafeOpen: false,
+  };
+
+  toggleCafeOpeningHandler = () => {
+    const isOpen = this.state.cafeOpen;
+    this.setState({ cafeOpen: !isOpen });
+  };
+
+  // toggleButtonTitle = () => {
+  //   this.setState({ buttonTextValue: "Change my name back!" });
+  // };
+
+  nameChangeHandler = () => {
+    this.setState({
+      cats: [
+        { name: "Manuel", age: 2, activity: "rolling around" },
+        { name: "Friendo", age: 4, activity: "snoozing" },
+        { name: "Jeremy", age: 5, activity: "playing tetris" },
+        {
+          name: "Enrique",
+          age: 10,
+          activity: "antifascist organising",
+        },
+        { name: "Lil Trish", age: 14, activity: "licking my butt" },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>The Cat Café</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to The Cat Café, where I will learn how to use props and state
+          and get to know some cats
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>Let's go!</p>
+
+        <button onClick={this.toggleCafeOpeningHandler}>
+          Open The Cat Café
+        </button>
+
+        {this.state.cafeOpen ? (
+          <div>
+            <Cat
+              name={this.state.cats[0].name}
+              age={this.state.cats[0].age}
+              activity={this.state.cats[0].activity}
+              nameChange={this.nameChangeHandler}
+              // buttonTextValue={this.toggleButtonTitle}
+            />
+            <Cat
+              name={this.state.cats[1].name}
+              age={this.state.cats[1].age}
+              activity={this.state.cats[1].activity}
+              nameChange={this.nameChangeHandler}
+              // buttonTextValue={this.toggleButtonTitle}
+            />
+            <Cat
+              name={this.state.cats[2].name}
+              age={this.state.cats[2].age}
+              activity={this.state.cats[2].activity}
+              nameChange={this.nameChangeHandler}
+              // buttonTextValue={this.toggleButtonTitle}
+            />
+            <Cat
+              name={this.state.cats[3].name}
+              age={this.state.cats[3].age}
+              activity={this.state.cats[3].activity}
+              nameChange={this.nameChangeHandler}
+              // buttonTextValue={this.toggleButtonTitle}
+            />
+            <Cat
+              name={this.state.cats[4].name}
+              age={this.state.cats[4].age}
+              activity={this.state.cats[4].activity}
+              nameChange={this.nameChangeHandler}
+              // buttonTextValue={this.toggleButtonTitle}
+            />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Cat/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const Cat = ({ name, age, activity, nameChange, buttonTextValue }) => {
+  return (
+    <div className="Cat">
+      <p>
+        My name is {name} and I am {age} years old.
+      </p>
+      <p>My favourite activity is {activity}!</p>
+      <button onClick={nameChange}>Change my name!</button>
+    </div>
+  );
+};
+
+export default Cat;
